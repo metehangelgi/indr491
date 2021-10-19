f8df1d3a0899abe36fdfd12b6e7807c6274450fb
htekin17 <92820745+htekin17@users.noreply.github.com>
2021-10-19 21:39:27 +0300

    test-hamide

---
+++ deneme1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 21:20:45 2021
+
+@author: hteki
+"""
+
+import deneme1method as d
+
+
+veri =d. MA([1,2,3])
+a=veri['a']
+print(a)
---
+++ deneme1method.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 21:17:15 2021
+
+@author: hteki
+"""
+
+def MA(veri):
+    returnValues={'a':10,'b':20}
+    return returnValues
